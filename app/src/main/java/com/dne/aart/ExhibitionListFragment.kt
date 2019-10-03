45a588e98ff445f2ede0a1234e4bcf2be715eda9
hamedshahidi <hamed.shahidiz@gmail.com>
2019-10-03 18:59:20 +0300

    Navigates to correct destination based on signed in status

--- app/src/main/java/com/dne/aart/ExhibitionListFragment.kt
+++ app/src/main/java/com/dne/aart/ExhibitionListFragment.kt
@@ -1,18 +1,16 @@
 package com.dne.aart
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_sign_up.view.*
 
 
-
-
-
-class startFragment : Fragment() {
+class ExhibitionListFragment : Fragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +22,10 @@ class startFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        return inflater.inflate(R.layout.fragment_exhibition_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/com/dne/aart/MainActivity.kt
+++ app/src/main/java/com/dne/aart/MainActivity.kt
@@ -2,11 +2,41 @@ package com.dne.aart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
+import androidx.core.view.isVisible
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_sign_up.*
+
+var signedUp: Boolean = false
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment ?: return
+        val navController = host.navController
+
+
+        when (checkSignedIn()) {
+            true -> findNavController(R.id.nav_host_fragment).navigate(R.id.exhibitionListFragment)
+            false -> {
+                // Hide bottom nav bar if user is not signed in
+                bottom_nav_view.visibility = GONE
+                // Navigate to sign up page
+                findNavController(R.id.nav_host_fragment).navigate(R.id.signUpFragment)
+            }
+        }
+    }
+
+    private fun checkSignedIn(): Boolean {
+        //TODO() check shared preferences for username. Return true if find one.
+        return true
     }
 }
