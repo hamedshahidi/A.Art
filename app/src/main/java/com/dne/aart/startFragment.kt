adcba8eeda004797d3ee59216055be0420f40a7f
hamedshahidi <hamed.shahidiz@gmail.com>
2019-10-03 17:48:48 +0300

    Navigation base implemented

---
+++ app/src/main/java/com/dne/aart/startFragment.kt
@@ -0,0 +1,29 @@
+package com.dne.aart
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+
+
+class startFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+}
