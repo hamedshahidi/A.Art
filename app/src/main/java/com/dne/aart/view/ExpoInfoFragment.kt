a1efd060946ae356b168aee684ba3cbfb90ad11d
hamedshahidi <hamed.shahidiz@gmail.com>
2019-10-08 11:24:52 +0300

    location map fragment added to expo info page

--- app/src/main/java/com/dne/aart/view/ExpoInfoFragment.kt
+++ app/src/main/java/com/dne/aart/view/ExpoInfoFragment.kt
@@ -10,38 +10,66 @@ import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.dne.aart.MapsActivity
-import com.dne.aart.R
 import com.dne.aart.util.DataManager
 import kotlinx.android.synthetic.main.fragment_expo_info.*
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.CameraPosition
 
 class ExpoInfoFragment : Fragment() {
 
     private var expoId = 0
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(com.dne.aart.R.layout.fragment_expo_info, container, false)
+
         val safeArgs: ExpoInfoFragmentArgs by navArgs()
         expoId = safeArgs.expoId
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expo_info, container, false)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val expo = DataManager.getExpoById(expoId)
-        tv_title_expoinfo.text = expo.title
-        tv_info_expoinfo.text = expo.info
-
         val lat = expo.location.lat
         val lon = expo.location.long
+
+        // ==================== MAP START =====================
+        val mapFragment =
+            childFragmentManager.findFragmentById(com.dne.aart.R.id.mapFragment)
+                    as SupportMapFragment?
+        mapFragment!!.getMapAsync { mMap ->
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            mMap.clear() //clear old markers
+            val googlePlex = CameraPosition.builder()
+                .target(LatLng(lat, lon))
+                .zoom(17f)
+                .bearing(0f)
+                .tilt(45f)
+                .build()
+
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 1000, null)
+
+            val marker = MarkerOptions()
+                .position(LatLng(lat, lon))
+                .title(expo.title)
+            mMap.setOnMarkerClickListener {
+                false
+            }
+            mMap.addMarker(marker).showInfoWindow()
+        }
+        // ==================== MAP END =======================
+
+        tv_title_expoinfo.text = expo.title
+        tv_info_expoinfo.text = expo.info
 
         btn_expand_map.setOnClickListener {
             val intent = Intent(activity!!.applicationContext, MapsActivity::class.java)
@@ -51,7 +79,7 @@ class ExpoInfoFragment : Fragment() {
             startActivity(intent)
         }
 
-        val btnSelectModel = view.findViewById<Button>(R.id.btn_select_model)
+        val btnSelectModel = view.findViewById<Button>(com.dne.aart.R.id.btn_select_model)
         btnSelectModel.setOnClickListener {
             val action =
                 ExpoInfoFragmentDirections.actionExpoInfoFragmentToArtListFragment()
